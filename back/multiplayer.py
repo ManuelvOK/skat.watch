0bb8769b969b6ca50949804cc99b9754c11f0310
Leon Georgi <leon@ifsr.de>
2020-04-05 22:22:07 +0200

    Sort quarantine cards

--- back/multiplayer.py
+++ back/multiplayer.py
@@ -1,39 +1,27 @@
 import random
 
-cards = [ "Schell 7",
-          "Schell 8",
-          "Schell 9",
-          "Schell 10",
-          "Schell Unter",
-          "Schell Ober",
-          "Schell König",
-          "Schell Ass",
-          "Herz 7",
-          "Herz 8",
-          "Herz 9",
-          "Herz 10",
-          "Herz Unter",
-          "Herz Ober",
-          "Herz König",
-          "Herz Ass",
-          "Blatt 7",
-          "Blatt 8",
-          "Blatt 9",
-          "Blatt 10",
-          "Blatt Unter",
-          "Blatt Ober",
-          "Blatt König",
-          "Blatt Ass",
-          "Eichel 7",
-          "Eichel 8",
-          "Eichel 9",
-          "Eichel 10",
-          "Eichel Unter",
-          "Eichel Ober",
-          "Eichel König",
-          "Eichel Ass" ]
+colors = [
+    "Schell",
+    "Herz",
+    "Blatt",
+    "Eichel"
+]
+
+values = [
+    "7",
+    "8",
+    "9",
+    "10",
+    "Unter",
+    "Ober",
+    "König",
+    "Ass"
+]
+
 
 def get_hand(player, seed=1337):
     random.seed(seed)
-    current_game_cards = random.sample(cards, k=len(cards))
-    return current_game_cards[player*10:player*10+10]
+    current_game_cards = random.sample(range(32), k=32)
+    player_cards = current_game_cards[player * 10:player * 10 + 10]
+    player_cards.sort()
+    return [f"{colors[card // 8]} {values[card % 8]}" for card in player_cards]
