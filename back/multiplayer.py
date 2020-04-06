8e0e206a7d6225847cecf13cb095028f56754ef0
Leon Georgi <leon@ifsr.de>
2020-04-06 19:42:41 +0200

    Remove magic numbers

--- back/multiplayer.py
+++ back/multiplayer.py
@@ -17,11 +17,21 @@ values = [
     "König",
     "Ass"
 ]
+color_count = len(colors)
+value_count = len(values)
+total_card_count = color_count * value_count
+
+
+def get_card(card_id):
+    return f"{colors[card_id // value_count]} {values[card_id % value_count]}"
 
 
 def get_hand(player, seed=1337):
     random.seed(seed)
-    current_game_cards = random.sample(range(32), k=32)
-    player_cards = current_game_cards[player * 10:player * 10 + 10]
-    player_cards.sort()
-    return [f"{colors[card // 8]} {values[card % 8]}" for card in player_cards]
+    # Shuffled list of all card identifiers
+    current_game_cards = random.sample(range(total_card_count), k=total_card_count)
+
+    # Pick 10 cards for the specified player
+    player_cards = sorted(current_game_cards[player * 10:player * 10 + 10])
+
+    return [get_card(card_id) for card_id in player_cards]
