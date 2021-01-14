fd33b6add068fdc360e280a9a9429c729be9a0b3
Leon Georgi <leon@ifsr.de>
2021-01-14 19:39:56 +0100

    Change design to match skat.watch

--- static/js/quarantine.js
+++ static/js/quarantine.js
@@ -1,6 +1,6 @@
 function toggleView(item) {
     var x = document.getElementById(item);
-    if (x.style.display == "block") {
+    if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
